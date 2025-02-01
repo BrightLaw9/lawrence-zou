6a8c95aa5c8c21e7c8e8f962f17874eac61c5ec8
BrightLaw9 <46764863+BrightLaw9@users.noreply.github.com>
2025-02-01 01:49:12 -0500

    Fix navbar, intro, root colors

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 
-import styles from "./Intro.module.css";
+import "./Intro.css";
 import { getImageUrl } from "../../utils";
+import { Mail } from "lucide-react";
 
 export const Intro = () => {
   return (
-    <section className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Lawrence!</h1>
-        <p className={styles.description}>
+    <section className="intro-section">
+      <div className="content">
+        <h1 className="title">Hi, I'm Lawrence!</h1>
+        <p className="description">
           I'm a full-stack developer with interest in AI and evolving technologies!
           I have with 5 years of experience with Python and Java. Feel free to reach out, happy to chat!
         </p>
-        <a href="mailto:lawrencezou3s@gmail.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className="button-container">
+          <a href="mailto:lawrencezou3s@gmail.com" className="contact-button">
+            <Mail />
+            Let's Connect
+          </a>
+        </div>
       </div>
-      <img
-        src={getImageUrl("hero/LawrenceProfileRounded.png")}
-        //src={"/assets/hero/heroImage.png"}
-        alt="Hero image of me"
-        className={styles.heroImg}
-      />
-      <div className={styles.topBlur} />
-      <div className={styles.bottomBlur} />
+      <div className="profile-image-container">
+        <img
+          src={getImageUrl("hero/LawrenceProfileRounded.png")}
+          alt="Lawrence's Profile Photo"
+          className="profile-image"
+        />
+      </div>
     </section>
   );
 };
